4a4eb075393510e72841291e81d552aaa856b08d
Jose Ulloa <jandres258@hotmail.com>
2020-07-23 20:56:42 -0600

    editar consecutivos

--- admin/src/app/pages/crear-consecutivos/crear-consecutivos.component.ts
+++ admin/src/app/pages/crear-consecutivos/crear-consecutivos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ConsecutivosService } from 'src/app/services/consecutivos.service';
 import { AlertService } from 'src/app/services/alert.service';
 import { Consecutivo } from 'src/app/models/consecutivo';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-crear-consecutivos',
@@ -10,22 +11,34 @@ import { Consecutivo } from 'src/app/models/consecutivo';
 })
 export class CrearConsecutivosComponent implements OnInit {
 
-  objeto: Consecutivo = new Consecutivo();
-  constructor(private servicio:ConsecutivosService, private alert: AlertService ) { }
+  id: string;
+  esCreacion: boolean = true;
+  objeto: Consecutivo = new Consecutivo()
+
+  constructor(private servicio: ConsecutivosService,private route: ActivatedRoute, private alert: AlertService ) { }
   
 
 
   ngOnInit() {
-
-
-    
+    this.id = this.route.snapshot.paramMap.get('id')
+    this.servicio.obtenerUno(this.id).subscribe(data => {
+      this.objeto = data
+      this.esCreacion = false
+    })
   }
 
   submit() {
     this.alert.showLoading()
+    if(this.esCreacion) {
       this.servicio.insertar(this.objeto).subscribe(response => {
-      this.alert.success('Elemento creado correctamente!');
-    }, this.alert.handleError)
+        this.alert.success('Elemento creado correctamente!');
+      }, err => this.alert.handleError(err))
+    } else {
+      this.servicio.modificar(this.objeto).subscribe(response => {
+        this.alert.success('Elemento actualizado correctamente!');
+      }, err => this.alert.handleError(err))
+    }
+    
   }
 
 }
