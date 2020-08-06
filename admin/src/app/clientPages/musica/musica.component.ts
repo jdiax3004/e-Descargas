cca9a1ff88808438a4121a5d7d48284b2c74a976
Alejandro Salguero <alejandrosalgueroq@hotmail.com>
2020-08-05 20:10:20 -0600

    Filtros en páginas de cliente

--- admin/src/app/clientPages/musica/musica.component.ts
+++ admin/src/app/clientPages/musica/musica.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { GeneroMusicaService } from '../../services/genero-musica.service';
+import { IdiomaService } from '../../services/idioma.service';
+import { AlertService } from '../../services/alert.service';
+import { Musica } from '../../../../../shared/app/models/musica';
+import { MusicaService } from '../../services/musica.service';
 
 @Component({
   selector: 'app-musica',
@@ -8,12 +12,52 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 })
 export class MusicaComponent implements OnInit {
 
-  constructor(private modalService: NgbModal) { }
-
+  constructor(private generoService: GeneroMusicaService, private idiomasService: IdiomaService, private alert: AlertService,
+               private musica: MusicaService) { }
+  objetos: Musica[] = [];
+  filtros: any = {};
+  generos: any[] = [];
+  idiomas: any[] = [];
   ngOnInit() {
+    this.cargarGeneros();
+    this.cargarIdiomas();
+    //this.cargar()
   }
 
-  open(content) {
-    this.modalService.open(content);
+  cargarGeneros() {
+    this.generoService.obtener().subscribe(result => {
+      this.generos = result
+    })
+  }
+
+  cargarIdiomas() {
+    this.idiomasService.obtener().subscribe(result => {
+      this.idiomas = result;
+    })
+  }
+
+  cargar() {
+    console.log(this.filtros);
+    if(!this.verificarParametros(this.filtros)) {
+      this.alert.error('No ingreso parametros');
+      return;
+    }
+    this.alert.showLoading()
+    this.musica.obtener(this.filtros).subscribe(data => {
+      this.objetos = data;
+      console.log(this.objetos);
+      this.alert.hideLoading();
+    })
+  }
+
+  verificarParametros(obj) {
+    if (Object.keys(obj).length === 0) return false;
+    for(let key of Object.keys(obj)) {
+      console.log(obj[key] !== "")
+      if(obj[key] !== "") {
+        return true;
+      }
+    }
+    return false;
   }
 }
