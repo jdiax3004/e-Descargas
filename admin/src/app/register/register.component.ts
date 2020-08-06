510f21c56c2a481f3c771fb6d99557511642ab3e
Julian Diaz <jdiazh362@ulacit.ed.cr>
2020-08-06 02:58:12 -0600

    Registro Cliente Listo

--- admin/src/app/register/register.component.ts
+++ admin/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from '../services/usuario.service';
+import { AlertService } from '../services/alert.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  objeto: Usuario = new Usuario();
+  constructor(private servicio:UsuarioService,private alert:AlertService,private router:Router) { }
 
   ngOnInit() {
+  }
+
+  submit() {
+    this.alert.showLoading()
+    this.servicio.insertar(this.objeto).subscribe(response => {
+      this.alert.success('Elemento creado correctamente!');
+      this.router.navigate(['/login']);
+    }, err => this.alert.handleError(err))
   }
 
 }
