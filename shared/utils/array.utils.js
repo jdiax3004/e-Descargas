56c1792adc68ef662c1ad4a15b788d09c00d912c
Jose Ulloa <jandres258@hotmail.com>
2020-08-06 00:09:07 -0600

    Arreglar filtros

--- shared/utils/array.utils.js
+++ shared/utils/array.utils.js
@@ -12,17 +12,21 @@ arrayUtils.filtrar = (array, filtros) => {
             let valor = filtros[filtro]
             if(valor) {
                 array = array.filter(obj => {
-                    let trydate = new Date(obj[filtro])
-                    if(!isNaN(trydate)) {
-                        try {
-                            return trydate.getTime() >= new Date(valor).getTime() 
-                        } catch(err) {
-                            return false
-                        }
-                    } else if(typeof obj[filtro] === 'string') {
+                    if(typeof obj[filtro] === 'string') {
                         return obj[filtro].includes(valor)
                     }
-                        
+
+                    if(filtro.includes('Fecha')) {
+                        let trydate = new Date(obj[filtro])
+                        if(!isNaN(trydate)) {
+                            try {
+                                return trydate.getTime() >= new Date(valor).getTime() 
+                            } catch(err) {
+                                return false
+                            }
+                        }
+                    }
+
                     return obj[filtro] == valor
                 })
             } 
